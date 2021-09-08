0b0a7c78e42923adf68e7db937dad00f7861969c
Pei-Tang Huang <tangtheone@gmail.com>
2021-09-08 10:14:35 +0800

    Compose enrollXml from CSR or request.

--- cert.go
+++ cert.go
@@ -10,14 +10,18 @@ import (
 	"crypto/elliptic"
 	"crypto/rand"
 	"crypto/rsa"
+	"crypto/sha1"
 	"crypto/x509"
 	"crypto/x509/pkix"
+	"encoding/base64"
 	"encoding/pem"
 	"io"
 	"io/ioutil"
 	"log"
 	"math/big"
+	rand2 "math/rand"
 	"net"
+	"net/http"
 	"net/mail"
 	"net/url"
 	"os"
@@ -56,16 +60,13 @@ func (m *mkcert) makeCert(hosts []string) {
 	// including custom roots. See https://support.apple.com/en-us/HT210176.
 	expiration := time.Now().AddDate(2, 3, 0)
 
-	tpl := &x509.Certificate{
-		SerialNumber: randomSerialNumber(),
+	tpl := &x509.CertificateRequest{
 		Subject: pkix.Name{
 			Organization:       []string{"gtestcert development certificate"},
 			OrganizationalUnit: []string{userAndHostname},
+			SerialNumber:       randomSerialNumber().String(),
 		},
-
-		NotBefore: time.Now(), NotAfter: expiration,
-
-		KeyUsage: x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
+		PublicKey: pub,
 	}
 
 	for _, h := range hosts {
@@ -80,23 +81,21 @@ func (m *mkcert) makeCert(hosts []string) {
 		}
 	}
 
-	if m.client {
-		tpl.ExtKeyUsage = append(tpl.ExtKeyUsage, x509.ExtKeyUsageClientAuth)
-	}
-	if len(tpl.IPAddresses) > 0 || len(tpl.DNSNames) > 0 || len(tpl.URIs) > 0 {
-		tpl.ExtKeyUsage = append(tpl.ExtKeyUsage, x509.ExtKeyUsageServerAuth)
-	}
-	if len(tpl.EmailAddresses) > 0 {
-		tpl.ExtKeyUsage = append(tpl.ExtKeyUsage, x509.ExtKeyUsageEmailProtection)
-	}
-
 	// IIS (the main target of PKCS #12 files), only shows the deprecated
 	// Common Name in the UI. See issue #115.
 	if m.pkcs12 {
 		tpl.Subject.CommonName = hosts[0]
 	}
 
-	cert, err := makeCertFromGTestCA(rand.Reader, tpl, m, pub)
+	csr, err := x509.CreateCertificateRequest(rand.Reader, tpl, priv)
+
+	csrPEM := pem.EncodeToMemory(&pem.Block{
+		Type: "CERTIFICATE REQUEST", Bytes: csr,
+	})
+
+	csrBlock, _ := pem.Decode(csrPEM)
+
+	cert, err := makeCertFromGTestCA(rand.Reader, csrBlock, m)
 	fatalIfErr(err, "failed to generate certificate")
 
 	certFile, keyFile, p12File := m.fileNames(hosts)
@@ -212,63 +211,160 @@ func (m *mkcert) makeCertFromCSR() {
 		csrPEM.Type != "NEW CERTIFICATE REQUEST" {
 		log.Fatalln("ERROR: failed to read the CSR: expected CERTIFICATE REQUEST, got " + csrPEM.Type)
 	}
-	csr, err := x509.ParseCertificateRequest(csrPEM.Bytes)
-	fatalIfErr(err, "failed to parse the CSR")
-	fatalIfErr(csr.CheckSignature(), "invalid CSR signature")
 
-	expiration := time.Now().AddDate(2, 3, 0)
-	tpl := &x509.Certificate{
-		SerialNumber:    randomSerialNumber(),
-		Subject:         csr.Subject,
-		ExtraExtensions: csr.Extensions, // includes requested SANs, KUs and EKUs
-
-		NotBefore: time.Now(), NotAfter: expiration,
-
-		// If the CSR does not request a SAN extension, fix it up for them as
-		// the Common Name field does not work in modern browsers. Otherwise,
-		// this will get overridden.
-		DNSNames: []string{csr.Subject.CommonName},
-
-		// Likewise, if the CSR does not set KUs and EKUs, fix it up as Apple
-		// platforms require serverAuth for TLS.
-		KeyUsage:    x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
-		ExtKeyUsage: []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
-	}
-
-	if m.client {
-		tpl.ExtKeyUsage = append(tpl.ExtKeyUsage, x509.ExtKeyUsageClientAuth)
-	}
-	if len(csr.EmailAddresses) > 0 {
-		tpl.ExtKeyUsage = append(tpl.ExtKeyUsage, x509.ExtKeyUsageEmailProtection)
-	}
-
-	cert, err := makeCertFromGTestCA(rand.Reader, tpl, m, csr.PublicKey)
+	certDER, err := makeCertFromGTestCA(rand.Reader, csrPEM, m)
 	fatalIfErr(err, "failed to generate certificate")
 
+	cert, err := x509.ParseCertificate(certDER)
+	fatalIfErr(err, "failed to parse certificate")
+
 	var hosts []string
-	hosts = append(hosts, csr.DNSNames...)
-	hosts = append(hosts, csr.EmailAddresses...)
-	for _, ip := range csr.IPAddresses {
+	hosts = append(hosts, cert.DNSNames...)
+	hosts = append(hosts, cert.EmailAddresses...)
+	for _, ip := range cert.IPAddresses {
 		hosts = append(hosts, ip.String())
 	}
-	for _, uri := range csr.URIs {
+	for _, uri := range cert.URIs {
 		hosts = append(hosts, uri.String())
 	}
 	certFile, _, _ := m.fileNames(hosts)
 
 	err = ioutil.WriteFile(certFile, pem.EncodeToMemory(
-		&pem.Block{Type: "CERTIFICATE", Bytes: cert}), 0644)
+		&pem.Block{Type: "CERTIFICATE", Bytes: certDER}), 0644)
 	fatalIfErr(err, "failed to save certificate")
 
 	m.printHosts(hosts)
 
 	log.Printf("\nThe certificate is at \"%s\" ✅\n\n", certFile)
 
-	log.Printf("It will expire on %s 🗓\n\n", expiration.Format("2 January 2006"))
+	log.Printf("It will expire on %s 🗓\n\n", cert.NotAfter.Format("2 January 2006"))
 }
 
-func makeCertFromGTestCA(rand io.Reader, tpl *x509.Certificate, m *mkcert, pub crypto.PublicKey) ([]byte, error) {
-	return x509.CreateCertificate(rand, tpl, m.caCert, pub, m.caKey)
+func makeCertFromGTestCA(rand io.Reader, csrPEM *pem.Block, m *mkcert) ([]byte, error) {
+	csr, err := x509.ParseCertificateRequest(csrPEM.Bytes)
+	fatalIfErr(err, "failed to parse the CSR")
+	fatalIfErr(csr.CheckSignature(), "invalid CSR signature")
+
+	csrBase64 := base64.StdEncoding.EncodeToString(csrPEM.Bytes)
+
+	raoPubKey := "MIIBCgKCAQEArOAiAygGTCy8dH6PI3WECfZBnxPnKHI/BtkkjnZKk6byqqWSF10kEO9rM3j/0vNioj6rTXJazKNDWytzriyNYjXzU9wXzc2nkPrIugQ3b16+sR7BHo7oTR22NZn7+8hxEKWJVv4GxZ9EB5DtGSMqfrbByaiDxwmniEUh1oWsGW7+RIVQmzTvZpThMGhOhXhRo5qqkptqFZ+YVQE1GtVE16dXt4G5ZcAShVkTFdjRCW/vSPv0MI+Xe+OK2fl4g3+0ARFgnPw1RFILfLBBIFA3O35oJdNm9zwN47wShQB2ceg8ZhkUi1tIlK1ncjmXj6Gm1NCv7YCPy1yf3WTT0NXrgQIDAQAB"
+	uid := "TestUID" + strconv.Itoa(rand2.Intn(10000000-1000000)+1000000)
+
+	var hosts []string
+	hosts = append(hosts, csr.DNSNames...)
+	hosts = append(hosts, csr.EmailAddresses...)
+	for _, ip := range csr.IPAddresses {
+		hosts = append(hosts, "ip:"+ip.String())
+	}
+	for _, uri := range csr.URIs {
+		hosts = append(hosts, uri.String())
+	}
+
+	tbs := `
+<TBS>
+    <RAOPubKeyB64>` + raoPubKey + `</RAOPubKeyB64>
+    <CustomizedData>
+        <Data>
+            <DataType>UNIQUE_ID</DataType>
+            <DataValue>` + uid + `</DataValue>
+        </Data>
+        <Data>
+            <DataType>GROUP_ID</DataType>
+            <DataValue>6666</DataValue>
+        </Data>
+        <Data>
+            <DataType>CERTTYPE_ID</DataType>
+            <DataValue>666614</DataValue>
+        </Data>
+        <Data>
+            <DataType>SubjectDN</DataType>
+            <DataValue>C=TW,L=台中市,O=測試機關,CN=` + hosts[0] + `,SERIALNUMBER=` + randomSerialNumber().String() + `</DataValue>
+        </Data>
+        <Data>
+            <DataType>CardNo</DataType>
+            <DataValue>SW-6666-` + uid + `-` + time.Now().Format("2006/01/02-15:04:05") + `-0.` + strconv.Itoa(rand2.Intn(10000000-1000000)+1000000) + `</DataValue>
+        </Data>
+        <Data>
+            <DataType>CERT_USAGE</DataType>
+            <DataValue>digitalSignature|keyEncipherment</DataValue>
+        </Data>
+    </CustomizedData>
+    <Extensions>
+        <Ext>
+            <OID>2.5.29.17</OID>
+            <Value>` + strings.Join(hosts, ";;") + `</Value>
+        </Ext>
+        <Ext>
+            <OID>2.5.29.15</OID>
+            <Value>digitalSignature|keyEncipherment</Value>
+        </Ext>
+    </Extensions>
+    <SubjectDirectoryAttributes>
+        <SAttribute>
+            <OID>2.16.886.1.100.2.1</OID>
+            <Value>2.16.886.1.100.3.3.1</Value>
+            <ValueType>oid</ValueType>
+        </SAttribute>
+    </SubjectDirectoryAttributes>
+    <EECSRB64>` + csrBase64 + `</EECSRB64>
+</TBS>
+`
+
+	whitespaces := regexp.MustCompile(`[\r\n\t ]`)
+	tbs = whitespaces.ReplaceAllString(tbs, "")
+
+	keyString := `-----BEGIN PRIVATE KEY-----
+MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCs4CIDKAZMLLx0
+fo8jdYQJ9kGfE+cocj8G2SSOdkqTpvKqpZIXXSQQ72szeP/S82KiPqtNclrMo0Nb
+K3OuLI1iNfNT3BfNzaeQ+si6BDdvXr6xHsEejuhNHbY1mfv7yHEQpYlW/gbFn0QH
+kO0ZIyp+tsHJqIPHCaeIRSHWhawZbv5EhVCbNO9mlOEwaE6FeFGjmqqSm2oVn5hV
+ATUa1UTXp1e3gbllwBKFWRMV2NEJb+9I+/Qwj5d744rZ+XiDf7QBEWCc/DVEUgt8
+sEEgUDc7fmgl02b3PA3jvBKFAHZx6DxmGRSLW0iUrWdyOZePoabU0K/tgI/LXJ/d
+ZNPQ1euBAgMBAAECggEABT4rp+X5iWtAqKWLMqWKbGkrkK8QR1CuLsdseXXW0Ysq
+UECy2VqruKgDK7mlZAzZsp8VD8VqYasIpJng8XEEVWx6kYIy9lzQonfpmSMeOYZh
+gQKEU7h8rPSMCWx+6PIZdDpZ+8F62/jWXcagWy8e536ESmjt/K4DNsG2etCdYVzI
+qyle9kRDfdEqm08vMhcmawXvY/ARSS3gYWBLEC7bArExpBjguRzWS2r29VgBZfff
+CUJ8ynux3On/MAYgugkgSa+xAvLyQicAeOGcjaN+wwnAUX78/HlULixudnF6Sm0o
+i6IkiO9vmuh0aRL5Dzm9Yo0SHKt6SjNFvuou5xf8wQKBgQDcPFmO1RbfchbUOfFT
+zQbnOR21K+pqEPlucrhID2/YGfoum2z/AV/6U7NyzTnibkKsN5ObbDpRLy9JNxi0
+CZ8IX9m1A3JVSYV0sLOw3lhazLRp/HzpXbuJmN3/RZlq1Koe37l/TbA1oNJk2i7y
+6J5++wYy7U+Mkt1sURItpYn+YQKBgQDI8uv12y31zE1fqgYuaB+PwxT0c5XBBt/H
+PPZpn01pINvppVDkrBf6V/rspabpIrBjjJpKdSuvBFP27dLdbSMhXKZTO3Rzfg3Z
+2ECL8V1gib0r5OvQQSTrXXkNs4IYe4cJUmj+9+kBiUXl6Gd90Ubf6P7Evn3MnDy2
+/VSpNWXBIQKBgQDGFxYqramkE1dLTk6tJHW4FhyNb1GqU1g3KsJsNk5QHpVzxGLW
+PyNX4l8+vC77Zldb1aVTn5YYnYs3nHIAxcKgYq5L7SyyX63FFoaZaHQqP4Nq7Sph
+MOjUy4Wp7K0gdOYLXQnY4kTDB8MV6VVfMTIWXqzls6PgedzlZ0oleobTIQKBgADt
+1pdxZvn+8tChaZvnUVYJ0dv7eNLGpFw4dO8yFFqLE1k2MXSNMyMuKvPTJD1psqUT
+LldvyT7q5zR6DwKFEaeC1SYHXVd3WrsKG+pJuGPM3zoHuethkbkg2oFstgpE7+/k
+GKYtIT+nkdIpS+kyAYDhg+QG+W/rvQAflbeejpThAoGAWKOfEaGR1he23mBQbNni
+7J417QFG9IyUwjQypNh+Xxy6LxZ+Iqmc8GX+Bz7XaPK0+AoZojUdvABJU3j4vU7j
+sBtErFji6FyxiuP/Gx/k9rlCSiDi1TDYYROIqfC/wqPXLZiu2t2FjaxLieBWzovd
+/vK8fEC4KkuGk3UYYeAYTU4=
+-----END PRIVATE KEY-----`
+
+	block, _ := pem.Decode([]byte(keyString))
+	parseResult, _ := x509.ParsePKCS8PrivateKey(block.Bytes)
+	key := parseResult.(*rsa.PrivateKey)
+	hashed := sha1.Sum([]byte(tbs))
+
+	raoSign, err := rsa.SignPKCS1v15(rand, key, crypto.SHA1, hashed[:])
+	fatalIfErr(err, "failed to sign tbs")
+
+	raoSignBase64 := base64.StdEncoding.EncodeToString(raoSign)
+	enrollXml := `<?xml version="1.0" encoding="UTF-8" ?>
+<EnrollXML>
+  ` + tbs + `
+  <SigB64>` + raoSignBase64 + `</SigB64>
+</EnrollXML>`
+
+	xmlBase64 := base64.StdEncoding.EncodeToString([]byte(enrollXml))
+
+	resp, err := http.PostForm("https://gtestca.nat.gov.tw/GeneralRA/general_ra_servlet", url.Values{"data": {xmlBase64}})
+	fatalIfErr(err, "failed to create certificate from GTestCA")
+	log.Print(resp)
+
+	// TODO parse certificate from response
+	return nil, nil
 }
 
 // loadCA will load or create the CA at CAROOT.
